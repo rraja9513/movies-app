d680f7cae977a3242fb4b4d5958c72b827e26681
rraja9513 <46923660+rraja9513@users.noreply.github.com>
2021-04-02 18:53:31 +0530

    Changed

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb+srv://admin-raja:7619224167@Job@cluster0.bw6nl.mongodb.net/cinema', { useNewUrlParser: true })
+    .connect('mongodb+srv://username:password@Job@cluster0.bw6nl.mongodb.net/cinema', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
